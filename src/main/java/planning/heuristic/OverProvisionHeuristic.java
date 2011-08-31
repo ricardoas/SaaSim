60776ef2e85695529587fe83ea514d3b603fc5e0
davidcmm <davidcmm@github.com>
2011-08-31 21:09:43 +0000

    Classes for other planning heuristics.

---
+++ src/main/java/planning/heuristic/OverProvisionHeuristic.java
@@ -0,0 +1,30 @@
+package planning.heuristic;
+
+import java.util.List;
+
+import commons.cloud.Provider;
+import commons.cloud.User;
+import commons.io.HistoryBasedWorkloadParser;
+
+public class OverProvisionHeuristic implements PlanningHeuristic{
+
+	@Override
+	public void findPlan(HistoryBasedWorkloadParser workloadParser,
+			List<Provider> cloudProviders, List<User> cloudUsers) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double getEstimatedProfit(int period) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<String> getPlan(List<User> cloudUsers) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
