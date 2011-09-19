efca9ce42987b60f75ce08842c2d51159c4885c1
davidcmm <davidcmm@github.com>
2011-09-19 21:03:03 +0000

    Private constructor Configuration

--- src/main/java/commons/config/Configuration.java
+++ src/main/java/commons/config/Configuration.java
@@ -237,7 +237,7 @@ public class Configuration	extends PropertiesConfiguration{
 	 * @param propertiesFileName
 	 * @throws ConfigurationException
 	 */
-	protected Configuration(String propertiesFileName) throws ConfigurationException {
+	private Configuration(String propertiesFileName) throws ConfigurationException {
 		super(propertiesFileName);
 		verifyProperties();
 		parseProperties();
