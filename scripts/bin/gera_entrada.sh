06377e6348419cadd93b49efff7585d7f16bc775
davidcmm <davidcmm@github.com>
2011-10-13 14:06:32 +0000

    Changing scripts arguments.

--- scripts/bin/gera_entrada.sh
+++ scripts/bin/gera_entrada.sh
@@ -1,8 +1,47 @@
 #!/bin/bash
+# Este script é responsável por requisitar a geração do trace a ser utilizado na simulação com base em pools de traces existentes.
 
-if [ $# -lt 3 ]; then
-        echo "usage: $0 -u=<number of saas clients> -d=<simulation period in days> -o=<output file> -p=<peak days>"
+if [ $# -lt 6 ]; then
+        echo "usage: $0 -u <number of saas clients> -d <simulation period in days> -o <output file> -p <peak days>"
         exit 1
+fi
+
+NUM_USERS=""
+SIM_PERIOD=""
+OUTPUT=""
+PEAK_DAYS=""
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -u )           		shift
+                                NUM_USERS=$1
+                                ;;
+	-d )    		shift
+				SIM_PERIOD=$1
+                                ;;
+	-o )    		shift
+				OUTPUT=$1
+                                ;;
+	-p )    		shift
+				PEAK_DAYS=$1
+                                ;;
+    esac
+    shift
+done
+
+if [ "${NUM_USERS}" == "" ]; then
+	echo "Invalid number of users"
+	exit 1
+fi
+
+if [ "${SIM_PERIOD}" == "" ]; then
+	echo "Invalid number of days to simulate"
+	exit 1
+fi
+
+if [ "${OUTPUT}" == "" ]; then
+	echo "Invalid output file"
+	exit 1
 fi
 
 
@@ -20,11 +59,12 @@ set_classpath() {
 
 find_self && set_classpath
 
+
 NORM_POOL="/home/david/workspace/SaaSim/norm"
 PEAK_POOL="/home/david/workspace/SaaSim/peak"
 TRANS_POOL="/home/david/workspace/SaaSim/trans"
 
-java -server -cp $CLASSPATH commons.util.UserPropertiesGenerator $1 $2 $3 ${NORM_POOL} ${TRANS_POOL} ${PEAK_POOL} $4
+java -server -cp $CLASSPATH commons.util.UserPropertiesGenerator ${NUM_USERS} ${SIM_PERIOD} ${OUTPUT} ${NORM_POOL} ${TRANS_POOL} ${PEAK_POOL} ${PEAK_DAYS}
 
 
 
--- scripts/bin/roda.sh
+++ scripts/bin/roda.sh
@@ -1,14 +1,41 @@
 #!/bin/bash
 # Esse script contem todo o fluxo de execucao do simulador
 
-if [ $# -lt 2 ]; then
-        echo "usage: $0 <number of saas clients> <simulation period in days>"
+if [ $# -lt 6 ]; then
+	echo "usage: $0 -u <number of saas clients> -d <simulation period in days> -p <peak days>"
         exit 1
 fi
 
-NUM_USERS=$1
-SIM_DAYS=$2
+NUM_USERS=""
+SIM_PERIOD=""
+PEAK_DAYS=""
 
-./gera_entrada.sh -d ${SIM_DAYS} -u ${NUM_USERS} -o users.properties -p 0
+while [ "$1" != "" ]; do
+    case $1 in
+        -u )           		shift
+                                NUM_USERS=$1
+                                ;;
+	-d )    		shift
+				SIM_PERIOD=$1
+                                ;;
+	-p )    		shift
+				PEAK_DAYS=$1
+                                ;;
+    esac
+    shift
+done
 
-./executaPlanning.sh users.properties ${SIM_DAYS}
+if [ "${NUM_USERS}" == "" ]; then
+	echo "Invalid number of users"
+	exit 1
+fi
+
+if [ "${SIM_PERIOD}" == "" ]; then
+	echo "Invalid number of days to simulate"
+	exit 1
+fi
+
+
+./gera_entrada.sh -d ${SIM_PERIOD} -u ${NUM_USERS} -o users.properties -p ${PEAK_DAYS}
+
+./executaPlanning.sh users.properties ${SIM_PERIOD}
