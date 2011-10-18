351f04e2d640e4da02243fad96ba1c18d6797838
ricardoas <ricardoas@github.com>
2011-10-18 21:09:58 +0000

    scripts and build

--- scripts/bin/saasim.sh
+++ scripts/bin/saasim.sh
@@ -4,5 +4,8 @@ for lib in lib/*; do
 	CLASSPATH="$CLASSPATH:$lib"
 done
 
-java -cp $CLASSPATH provisioning.Main $*
- 
+java -Xmx1024m -server -cp $CLASSPATH provisioning.Main $*
+
+while [ -f ".je.dat" ] ; do
+	java -Xmx1024m -server -cp $CLASSPATH provisioning.Main $*
+done
