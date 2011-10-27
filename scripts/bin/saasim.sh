d7ba55fbc80c89386236272207c4f95f9d314d54
ricardoas <ricardoas@github.com>
2011-10-27 21:21:59 +0000

    print

--- scripts/bin/saasim.sh
+++ scripts/bin/saasim.sh
@@ -4,8 +4,12 @@ for lib in lib/*; do
 	CLASSPATH="$CLASSPATH:$lib"
 done
 
+echo "dia: 1"
 java -Xmx1024m -server -cp $CLASSPATH provisioning.Main $*
 
+i=1
 while [ -f ".je.dat" ] ; do
+	echo "dia: $i"
+	i=$(($i+1))
 	java -Xmx1024m -server -cp $CLASSPATH provisioning.Main $*
 done
