b5fe2e1fde491f9e7a01fd11b4299884b1ecc000
ricardoas <ricardoas@github.com>
2011-11-16 15:09:08 +0000

    scripts

--- src/main/java/commons/cloud/MachineType.java
+++ src/main/java/commons/cloud/MachineType.java
@@ -9,13 +9,15 @@ package commons.cloud;
  */
 public enum MachineType{
 
-	T1_MICRO(2, 1),
 	M1_SMALL(1, 1), 
 	M1_LARGE(2, 2), 
-	C1_MEDIUM(2.5, 2), 
 	M1_XLARGE(2, 4), 
-	M2_XLARGE(3.25, 2), M2_2XLARGE(3.25, 4), M2_4XLARGE(3.25, 4),
-	C1_XLARGE(2.5, 8);
+	C1_MEDIUM(2.5, 2), 
+	C1_XLARGE(2.5, 8),
+	M2_XLARGE(3.25, 2), 
+	M2_2XLARGE(3.25, 4), 
+	M2_4XLARGE(3.25, 4),
+	T1_MICRO(2, 1);
 	
 	private final double power;
 	private final int numberOfCores;
