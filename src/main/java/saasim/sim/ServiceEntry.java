d548ced334673ce13c7603d4806cf2ced2d95ad5
Ricardo Araújo Santos <rikardoas@gmail.com>
2012-12-26 21:53:59 -0300

    deleting old stuff

--- src/main/java/saasim/sim/ServiceEntry.java
+++
@@ -1,9 +0,0 @@
-package saasim.sim;
-
-import saasim.sim.core.EventHandler;
-
-public interface ServiceEntry extends EventHandler{
-	
-	void config(int d); 
-	
-}
--- src/main/resources/scripts/bin/run-request.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-url=$1
-report_file=$2
-
-( time wget -O /dev/null ${url} 2>&1 | grep "200 OK" | wc -l >> ok ) 2>&1 | tail -3 | head -1 | awk -F [^0-9] '{print 60000 * $6 + 1000 * $7 + $8}' >> ${report_file}
