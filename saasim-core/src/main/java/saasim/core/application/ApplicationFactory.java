94ac89c481f50232ddffbd1d29f90e2a4c4c6d43
Ricardo Araújo Santos <phd.ricardo@gmail.com>
2014-03-31 18:56:03 -0300

    single tier application factory

--- saasim-core/src/main/java/saasim/core/application/ApplicationFactory.java
+++ saasim-core/src/main/java/saasim/core/application/ApplicationFactory.java
@@ -1,36 +1,15 @@
 package saasim.core.application;
 
-import saasim.core.config.Configuration;
-import saasim.core.config.SaaSAppProperties;
 
 /**
  * Application assembler.
  *  
  * @author Ricardo Ara&uacute;jo Santos - ricardo@lsd.ufcg.edu.br
  */
-public abstract class ApplicationFactory {
+public abstract class ApplicationFactory extends AbstractFactory<Application> {
 	
-	/**
-	 * Unique instance
+	/* (non-Javadoc)
+	 * @see saasim.core.application.AbstractFactory#buildApplication()
 	 */
-	private static ApplicationFactory instance;
-	
-	/**
-	 * Starts up factory.
-	 * 
-	 * @return {@link ApplicationFactory} instance ready to assemble.
-	 */
-	public static ApplicationFactory getInstance(Configuration config) {
-		if(instance == null){
-			String className = config.getString(SaaSAppProperties.APPLICATION_FACTORY);
-			try {
-				instance = (ApplicationFactory) Class.forName(className).newInstance();
-			} catch (Exception e) {
-				throw new RuntimeException("Something went wrong when loading "+ className, e);
-			}
-		}
-		return instance;
-	}
-	
-	public abstract Application buildApplication();
+	public abstract Application build();
 }
