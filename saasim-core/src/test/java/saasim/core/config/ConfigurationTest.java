6bc1c65320bf955bcab87fb9463c9f54ddc5eb5d
Ricardo Araújo <phd.ricardo@gmail.com>
2014-05-25 19:21:44 -0300

    configuration test

--- saasim-core/src/test/java/saasim/core/config/ConfigurationTest.java
+++ saasim-core/src/test/java/saasim/core/config/ConfigurationTest.java
@@ -2,49 +2,41 @@ package saasim.core.config;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
 
 import org.apache.commons.configuration.ConfigurationException;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 /**
  */
+@RunWith(Parameterized.class)
 public class ConfigurationTest {
 	
-	private static final String SINGLE_TENANT_SINGLE_TIER_CONFIGURATION = "src/test/resources/single_tenant_single_tier_configuration.properties";
-	private static final String MULTI_TENANT_SINGLE_APPLICATION_SINGLE_TIER_CONFIGURATION = "src/test/resources/multi_tenant_single_application_single_tier_configuration.properties";
-	private static final String MULTI_TENANT_MULTI_APPLICATION_SINGLE_TIER_CONFIGURATION = "src/test/resources/multi_tenant_multi_application_single_tier_configuration.properties";
+	private static final String SRC_TEST_RESOURCES = "src/test/resources/";
+	private String file;
 
-	@BeforeClass
-	public static void setUpBefore(){
-		assert !new File("test.properties").exists() || new File("saasim.properties").delete(): "Unwanted file saasim.properties on root directory. Please delete it!";
-	}
 
-	@AfterClass
-	public static void tearDownAfter(){
-		assert !new File("test.properties").exists() || new File("saasim.properties").delete(): "Unwanted file saasim.properties on root directory. Please delete it!";
-	}
-
-	/**
-	 * Test method for {@link saasim.core.config.Configuration#Configuration()}.
-	 * @throws ConfigurationException 
-	 */
-	@Test(expected=ConfigurationException.class)
-	public void testConfigurationWithoutFile() throws ConfigurationException {
-		new Configuration("test.properties");
-	}
-
-	/**
-	 * Test method for {@link saasim.core.config.Configuration#Configuration()}.
-	 * @throws ConfigurationException 
-	 * @throws IOException 
-	 */
-	@Test
-	public void testConfigurationWithEmptyFile() throws ConfigurationException, IOException {
-		assert new File("test.properties").createNewFile(): "Could not create a file for testing purposes, verify permission.";
-		new Configuration("test.properties");
-		assert new File("test.properties").delete(): "Could not delete file for testing purposes, verify permission.";
+	@Parameters
+    public static Collection<Object[]> data() {
+    	File[] listOfFiles = new File(SRC_TEST_RESOURCES).listFiles();
+    	Object[][] filesnames = new String[listOfFiles.length][1]; 
+    	for (int i = 0; i < listOfFiles.length; i++) {
+    		try {
+				filesnames[i][0] = listOfFiles[i].getCanonicalPath();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+    	
+        return Arrays.asList(filesnames);
+    }
+    
+    public ConfigurationTest(String file) {
+		this.file = file;
 	}
 	
 	/**
@@ -53,28 +45,8 @@ public class ConfigurationTest {
 	 * @throws IOException 
 	 */
 	@Test
-	public void testSingleTenantSingleTierConfiguration() throws ConfigurationException, IOException {
-		new Configuration(SINGLE_TENANT_SINGLE_TIER_CONFIGURATION);
-	}
-
-	/**
-	 * Test method for {@link saasim.core.config.Configuration#Configuration()}.
-	 * @throws ConfigurationException 
-	 * @throws IOException 
-	 */
-	@Test
-	public void testMultiTenantSingleApplicationSingleTierConfiguration() throws ConfigurationException, IOException {
-		new Configuration(MULTI_TENANT_SINGLE_APPLICATION_SINGLE_TIER_CONFIGURATION);
-	}
-
-	/**
-	 * Test method for {@link saasim.core.config.Configuration#Configuration()}.
-	 * @throws ConfigurationException 
-	 * @throws IOException 
-	 */
-	@Test
-	public void testMultiTenantMultiApplicationSingleTierConfiguration() throws ConfigurationException, IOException {
-		new Configuration(MULTI_TENANT_MULTI_APPLICATION_SINGLE_TIER_CONFIGURATION);
+	public void testConstructor() throws ConfigurationException {
+		new Configuration(file);
 	}
 
 }
